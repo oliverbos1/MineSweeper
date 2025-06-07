eaf3b5db761812d4bae363ebef3b997f07e1353b
BabyMakingJuice <oliver.bos1@gmail.com>
2025-06-07 19:11:51 +0200

    changes for packaging

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,3 @@
 open module com.oliverbos1.main {
     requires com.almasb.fxgl.all;
-    requires com.almasb.fxgl.core;
-    requires com.almasb.fxgl.entity;
-    requires javafx.base;
-    requires javafx.graphics;
 }
