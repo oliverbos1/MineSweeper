f5acfc33ff3dc92af254a26562d6854ebb034ad4
BabyMakingJuice <oliver.bos1@gmail.com>
2025-06-09 19:21:33 +0200

    difficulty multipliers adjusted

--- src/main/java/com/oliverbos1/minesweeper/GameLogic.java
+++ src/main/java/com/oliverbos1/minesweeper/GameLogic.java
@@ -50,8 +50,8 @@ public class GameLogic {
     private int calcMineAmount(MineSweeperSettings settings) {
         return (int) Math.round(settings.nrTilesHorizontal() * settings.nrTilesHorizontal() * switch (settings.difficulty()) {
             case EASY -> 0.15625;
-            case MEDIUM -> 0.2;
-            case HARD -> 0.3;
+            case MEDIUM -> 0.19531;
+            case HARD -> 0.23438;
         });
     }
 
